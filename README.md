1ba65378fc1c381120664fc544d92d34ab0d084e
tnandee <tnandee@gmail.com>
2017-06-25 16:41:19 +0200

    display updates
    Ascii table added, display refresh interval modified

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cc_cv
 Custom firmware for: "Max 5A Adjustable CC CV Step-Down Charge Module Digital Voltmeter Ammeter Display LED"
+
+Tools needed:
+* Cosmic C compiler. I used Cosmi CxSTM8 Eval Kit v4.3.7
+* ST Visual Develop 
+* ST Visual Programmer
+* ST Link V2
--- main.c
+++ main.c
@@ -45,7 +45,7 @@ unsigned int readAda(int ch)
 }
 
 
-unsigned char dec2seg7(unsigned char d)
+unsigned char asc2seg7(unsigned char d)
 {
 	unsigned char segments[] = {//abcdefgp
 															0b11111100, // 0
@@ -59,8 +59,39 @@ unsigned char dec2seg7(unsigned char d)
 															0b11111110, // 8
 															0b11110110, // 9
 															0b00000000, // space
+															0b11101110, // A
+															0b00111110, // b
+															0b10011100, // C
+															0b01111010, // d
+															0b10011110, // E
+															0b10001110, // F
+															0b10111110, // G
+															0b01101110, // H
+															0b00001100, // I
+															0b11110000, // J
+															0b01101110, // K
+															0b00011100, // L
+															0b01101110, // M
+															0b00101010, // n
+															0b00111010, // o
+															0b11001110, // P
+															0b11111101, // Q
+															0b00001010, // r
+															0b10110110, // S, 5
+															0b00011110, // t
+															0b00111000, // u
+															0b01111100, // V, U
+															0b01111100, // W, V
+															0b01101110, // X
+															0b01001110, // y
+															0b11011010, // Z, 2
+
 															};
-	return d < 0x0B ? segments[d] : segments[d % 0x0B] | 0x01;
+	if (d >= '0' && d <='9'){ d = d - '0'; }
+	else if (d == ' '){ d = 10; }
+	else if (d >= 'A' && d <= 'Z'){ d = d - 'A' + 11; }
+	else if (d >= 100) {return segments[d - 100] | 0x01;}
+	return segments[d];
 }
 
 void display_digit(unsigned char digit)
@@ -84,7 +115,7 @@ void display_digit(unsigned char digit)
 							   1<<3}; // PA3						
 	int i;
 	unsigned char mask = 0x80;
-	digit = dec2seg7(digit);
+	digit = asc2seg7(digit);
 	
 	for(i=0; i<=7; i++){
 		if ((unsigned char)(digit & mask) > 0){
@@ -105,23 +136,24 @@ void refreshDisplay(unsigned char* digitVals)
 	PA_ODR |= (unsigned char)1<<2; // DSA, DSB
 	for(digitId = 0; digitId <= 7; digitId++){
 		PA_ODR &= (unsigned char)~(1<<1); // CP
-		display_digit(10);
-		myDelay(150);
+		display_digit(' ');
+		myDelay(10);
 		PA_ODR |= (unsigned char)(1<<1); // CP
 		myDelay(1);
 		PA_ODR &= (unsigned char)~(1<<2); // DSA, DSB
 		display_digit(*(digitVals + map[digitId]));
 		myDelay(1);
+	  display_digit(' ');
+		myDelay(10);
 	}
 }
 
 void formatDisplay(char* in_str, int offset, unsigned char* out_data)
 {
 			int i = 0;
-			while(*in_str)
-			{
+			while(*in_str){
 				if(*in_str == '.') 
-					{*(out_data + offset + i - 1) += 0x0B; in_str++;}
+					{*(out_data + offset + i - 1) += 100; in_str++;} // 100 is just a fake number
 				else 
 					{*(out_data + offset + i) = *in_str - '0'; i++; in_str++;}
 			}
@@ -134,25 +166,27 @@ int main(void)
 		float volt;
 		unsigned int sum_amp_raw = 0;
 		float amp;
-		unsigned char displayData[] = {0+11,1+11,2+11,3+11,4+11,5+11,6+11,7+11};
-		int i = 0;
+		unsigned char displayData[] = {'H', 'A', 'L', 'I', 'N', 'A', 'N', 'D'};
+		unsigned int i = 0;
+		unsigned int nAvg = 16;
 		char str_volt[10];
 		char str_amp[10];
 		char *ptr;
+		int refreshInterval = 50;
+		int refreshTimer = 0;
+		
 
 		GPIO_Config();
  
-		while(1)
-		{
+		while(1){
 			sum_volt_raw = sum_amp_raw = 0;
-			for(i=0; i<16; i++)
-			{
+			for(i=0; i<nAvg; i++){
 				sum_volt_raw += readAda(0x03);
 				sum_amp_raw += readAda(0x04);
 				myDelay(1);
 			}
-			sum_volt_raw = (int)(sum_volt_raw / 16);
-			sum_amp_raw = (int)(sum_amp_raw / 16);
+			sum_volt_raw = (int)(sum_volt_raw / nAvg);
+			sum_amp_raw = (int)(sum_amp_raw / nAvg);
 			
 			volt = (float)(((float)sum_volt_raw * 0.0403) + 0.13);
 			sprintf(str_volt, "%05.2f", (float)volt);
@@ -161,9 +195,14 @@ int main(void)
 			if (amp > 0.99) sprintf(str_amp, "%05.2f", (float)amp);
 			else sprintf(str_amp, "%05.3f", (float)amp);
 				
-			formatDisplay(str_volt, 0, displayData);
-			formatDisplay(str_amp, 4, displayData);
-
+			if (refreshTimer >= refreshInterval){
+				formatDisplay(str_volt, 0, displayData);
+				formatDisplay(str_amp, 4, displayData);
+				refreshTimer = 0;
+			}
+			else {
+				refreshTimer += 1;
+			}
 			refreshDisplay(displayData);
 		}
 }
